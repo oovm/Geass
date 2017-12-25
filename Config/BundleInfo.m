b9490c790f896d706242924517cc3112a60b4b1d
GalAster <280597828@qq.com>
2017-12-25 12:03:09 +0800

    架构::First Blood!

---
+++ Config/BundleInfo.m
@@ -0,0 +1,3 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{"RemovePaths" -> {"Private", ".git"}, "RemovePatterns" -> 
+  {"Packages/*.nb", "Packages/*/*.nb", "Packages/*/*/*.nb", ".DS_Store"}}
---
+++ PacletInfo.m
@@ -0,0 +1,3 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+Paclet[Name -> "Geis", Version -> "1.0.0", 
+ Extensions -> {{"Kernel", Root -> ".", Context -> {"Geis`"}}}]
