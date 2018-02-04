c72d604aaa66827f300c85682ebab0cd7799859e
GalAster <galaster@foxmail.com>
2018-02-04 19:51:35 +0800

    重构:: 去IDEA 去IDE

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,4 +1,13 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "Geis", Version -> "1.0.1", 
- Extensions -> {{"Kernel", "Root" -> ".", "Context" -> {"Geis`"}, 
-    Root -> ".", Context -> {"Geis`"}}}]
+Paclet[
+    Name -> "Geis",
+	Version -> "1.0.1",
+    Extensions -> {
+	    {"Kernel",
+		    "Root" -> ".",
+		    "Context" -> {"Geis`"},
+		    Root -> ".",
+		    Context -> {"Geis`"}
+	    }
+    }
+];
