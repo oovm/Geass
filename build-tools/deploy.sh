286d36b6d4a2bbd4fde9422b07269a990e74b2fa
GalAster <galaster@foxmail.com>
2018-03-12 21:27:17 +0800

    模块::打包器 PackPaclet

--- build-tools/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ../.git
-rm -rf ../.idea
-rm -rf ../Documentation
-rm -rf ../Packages/_*
-rm -rf ../Kernel/*.md
-rm -rf ../Kernel/*.wl
-rm -rf ../Kernel/*.nb
-rm -rf ../Kernel/*.sh
